aff9c1b8c63e6b66a7c4214aabe577c2e83264af
drumath <2237k.drumath@gmail.com>
2021-09-17 17:47:36 +0900

    :package: nugetからakdk関係のdllなどをインポート

---
+++ moveFile.bat
@@ -0,0 +1,8 @@
+MKDIR .\Assets\Plugins
+COPY .\External\Package\Microsoft.Azure.Kinect.Sensor.1.4.1\lib\native\amd64\release\depthengine_2_0.dll .\Assets\Plugins\
+COPY .\External\Package\Microsoft.Azure.Kinect.Sensor.1.4.1\lib\native\amd64\release\k4a.dll .\Assets\Plugins\
+COPY .\External\Package\Microsoft.Azure.Kinect.Sensor.1.4.1\lib\netstandard2.0\Microsoft.Azure.Kinect.Sensor.dll .\Assets\Plugins
+COPY .\External\Package\System.Memory.4.5.3\lib\netstandard2.0\System.Memory.dll .\Assets\Plugins\
+COPY .\External\Package\System.Numerics.Vectors.4.5.0\lib\netstandard2.0\System.Numerics.Vectors.dll .\Assets\Plugins\
+COPY .\External\Package\System.Runtime.CompilerServices.Unsafe.4.6.0\lib\netstandard2.0\System.Runtime.CompilerServices.Unsafe.dll .\Assets\Plugins\
+COPY .\External\Package\System.Buffers.4.5.1\lib\netstandard2.0\System.Buffers.dll .\Assets\Plugins\
